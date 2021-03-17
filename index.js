6c13977308e03bde2f1d51ef3132d3ca22d5dd4b
OCHADE EMMANUEL <eochade16@gmail.com>
2021-03-17 09:51:26 +0100

    traversed the dom using javaScript

---
+++ index.js
@@ -0,0 +1,19 @@
+const header = document.getElementById('main-header');
+header.style.backgroundColor = 'blue'
+
+//Parent Element
+
+const listItems = document.getElementById("items");
+
+//Logging listItems to the console to see what it outputs
+console.log(listItems.parentElement)
+
+listItems.parentElement.style.backgroundColor = '#f2f2f2';
+listItems.style.fontSize = '18px';
+
+//Using Query Selector to access html element
+
+const btn = document.querySelector('.btn');
+
+//Change the valeue of the button with class name of btn
+btn.style.backgroundColor = 'blue';
