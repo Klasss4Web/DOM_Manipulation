ba4badafd117442f1dce1643dacba84d5f8be3e2
OCHADE EMMANUEL <eochade16@gmail.com>
2021-03-19 11:53:56 +0100

    added interactivity to the UI

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const header = document.getElementById('main-header');
-header.style.backgroundColor = 'blue'
+header.style.backgroundColor = 'blue';
 
 //Parent Element
 
@@ -13,7 +13,37 @@ listItems.style.fontSize = '18px';
 
 //Using Query Selector to access html element
 
-const btn = document.querySelector('.btn');
+let btn = document.querySelector('.btn');
 
 //Change the color of the button with class name of btn
 btn.style.backgroundColor = 'blue';
+
+btn.addEventListener('click', function(e) {
+  let input = document.querySelector('.form-control').value;
+  let message = document.querySelector('.messageAlert');
+
+  if(input !== "") {
+    let listGroup = document.querySelector('.list-group');
+    let li = document.createElement('li');
+    li.className = 'list-group-item';
+    li.appendChild(document.createTextNode(input));
+    listGroup.appendChild(li);
+   
+  } else {
+    message.style.border = '3px solid red';
+    message.style.width = '300px';
+    message.style.padding = '10px';
+    message.textContent = 'please enter a valid input';
+    setTimeout(clearError, 2000)
+    
+  }
+  // console.log(listGroup);
+  // listGroup[0].textContent = input.value;
+  // console.log('loading...')
+  e.preventDefault()
+})
+
+let info = 'please enter a valid input'
+function clearError(){
+  document.querySelector('.messageAlert').remove();
+}
