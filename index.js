923e5dc9da0555644451c7db0e001c81b77c42dd
OCHADE EMMANUEL <eochade16@gmail.com>
2021-03-22 10:38:14 +0100

    added css file to this repo

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const header = document.getElementById('main-header');
 header.style.backgroundColor = 'blue';
 
+
+
 //Parent Element
 
 const listItems = document.getElementById("items");
@@ -19,22 +21,28 @@ let btn = document.querySelector('.btn');
 btn.style.backgroundColor = 'blue';
 
 btn.addEventListener('click', function(e) {
-  let input = document.querySelector('.form-control').value;
+  let input = document.querySelector('.form-control');
+  let inputValue = input.value;
   let message = document.querySelector('.messageAlert');
-
-  if(input !== "") {
+  let success = document.querySelector('.success')
+  if(inputValue !== "") {
     let listGroup = document.querySelector('.list-group');
     let li = document.createElement('li');
     li.className = 'list-group-item';
-    li.appendChild(document.createTextNode(input));
+    li.appendChild(document.createTextNode(inputValue));
     listGroup.appendChild(li);
+    success.style.display = 'block';
+    //success.textContent = 'please enter a valid input';
+    // setTimeout(clearMessage, 2000)
    
   } else {
     message.style.border = '3px solid red';
     message.style.width = '300px';
     message.style.padding = '10px';
     message.textContent = 'please enter a valid input';
-    setTimeout(clearError, 2000)
+    
+    
+    setTimeout(clearMessage, 2000)
     
   }
   // console.log(listGroup);
@@ -43,7 +51,8 @@ btn.addEventListener('click', function(e) {
   e.preventDefault()
 })
 
-let info = 'please enter a valid input'
-function clearError(){
+
+function clearMessage(){
   document.querySelector('.messageAlert').remove();
+  document.querySelector('.success').remove();
 }
